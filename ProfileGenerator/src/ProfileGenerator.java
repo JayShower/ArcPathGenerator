fe28d79fd535b409a404318cf4e78597680d7d73
Jay Schauer <lacrossejason@gmail.com>
2018-01-25 13:28:06 -0600

    Initial Commit

---
+++ ProfileGenerator/src/ProfileGenerator.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class ProfileGenerator {
+
+	public static Path generateProfile(List<Waypoint> waypoints, double pointDuration, double totalTime,
+			double maxAcceleration,
+			double maxVelocity) {
+
+		return null;
+	}
+}
---
+++ ProfileGenerator/src/TrajectoryPoint.java
@@ -0,0 +1,15 @@
+public class TrajectoryPoint {
+
+	public final double position;
+	public final double velocity;
+	public final double heading = 0;
+	public final double duration;
+
+	public TrajectoryPoint(double position, double velocity, double duration) {
+		super();
+		this.position = position;
+		this.velocity = velocity;
+		this.duration = duration;
+	}
+
+}
